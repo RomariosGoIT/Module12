7aaf054321886432ce892207dec8d5b7809173f8
RomariosGoIT <romantgt@gmail.com>
2018-07-31 09:30:03 +0300

    Update

--- Webinar2/src/index.js
+++ Webinar2/src/index.js
@@ -1,1 +1,8 @@
-console.log('Webpack is best of all!!');
+import './style.css';
+// import 'normalize.css'
+import './test.scss';
+import articleTlp from './templates/article.hbs';
+
+console.log(articleTlp)
+// import imageUrl from './assets/img.jpeg';
+// img.setAttribute('src', imageUrl);
