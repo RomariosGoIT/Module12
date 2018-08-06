03321eac6e0a97cc19b4a5beaf52a616df786069
RomariosGoIT <romantgt@gmail.com>
2018-08-06 07:14:34 +0300

    update

--- Webinar2/src/index.js
+++ Webinar2/src/index.js
@@ -1,19 +1,39 @@
 import './style.css';
-import {fetchImage} from './services/api';
+import {
+    fetchImage
+} from './services/api';
 import './test.scss';
-
+import * as storage from './services/storage'
 import articleTlp from './templates/article.hbs';
+
 
 
 const form = document.querySelector('.form')
 const input = document.querySelector('.input')
 const grid = document.querySelector('.grid')
-const spinner = document.querySelector('.spinner-overlay')
+const spinner = document.querySelector('.spinner-overlay');
+const persistedPhotos = storage.get()
+const fetchedPhotos = persistedPhotos ? persistedPhotos : [];
 
-const createImg = photos => {
+console.log(fetchedPhotos);
 
+const murkup = fetchPhotos(fetchedPhotos);
+
+updateGrid(murkup)
+
+function fetchPhotos(data) {
+    return data.reduce((markup, photos) => markup + articleTlp(photos), '')
+}
+
+function updateGrid (markup) {
+    grid.innerHTML += markup;
+}
+
+
+function createImg(photos) {
     grid.innerHTML = '';
     photos.hits.forEach(cont => {
+
         let content = articleTlp(cont);
         return grid.innerHTML += content;
     })
@@ -22,7 +42,11 @@ const createImg = photos => {
 const submitForm = event => {
     event.preventDefault()
     spinnerToggle()
+
     fetchImage(input.value, 18).then(photos => {
+
+        fetchedPhotos.push(...photos.hits);
+        storage.set(fetchedPhotos)
         createImg(photos);
         spinnerToggle();
     })
--- Webinar2/src/services/api.js
+++ Webinar2/src/services/api.js
@@ -3,11 +3,7 @@ import axios from 'axios';
 export const fetchImage = (query, count) => {
     const url = `https://pixabay.com/api/?key=9464143-b7cbb21d169259e439c99d06c&q=${query}&image_type=photo&per_page=${count}`
 
-    axios.get(url).then(response => console.log(response))
-
-
     return fetch(url).then(respone => {
-            console.log('fetch response:', respone)
             if (respone.ok) return respone.json()
             throw new Error('Error' + respone.statusText)
         })
